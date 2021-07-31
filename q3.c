8321139c40d28fb4f1d23d7609eecf3db3576c54
shrit_gautam <shrit2002@kgpian.iitkgp.ac.in>
2021-08-01 02:40:59 +0530

    Added new file

---
+++ q3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++){
+        printf("enter element no. %d: ",i+1);
+        scanf("%d",&a[i]);
+    }
+    int max=a[0];
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>=i;j--){
+            int sum=0;
+            for(int k=i;k<=j;k++){
+                sum+=a[k];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+    }
+    printf("max is: %d",max);
+
+
+
+    return 0;
+}
